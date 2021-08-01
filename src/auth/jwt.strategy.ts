ed11221ddc3d41f65bc31c07839d6f23480a0782
Vitor Gois <vitorcgois777@gmail.com>
2021-07-31 21:13:46 -0300

    Going live

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,9 +1,9 @@
-import { JwtPayload } from ".pnpm/@types+jsonwebtoken@8.5.4/node_modules/@types/jsonwebtoken";
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ExtractJwt, Strategy } from 'passport-jwt'
+import { JwtPayload } from "./interface/jwt-payload.interface";
 import { User } from "./user.entity";
 import { UsersRepository } from "./user.repository";
 
